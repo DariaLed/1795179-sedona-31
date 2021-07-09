670941eb6a4d257eb916bd251a949539a75e1f3e
DariaLed <84290710+DariaLed@users.noreply.github.com>
2021-07-09 11:54:44 +0300

    index, catalog revisions

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const formContainer = document.querySelector(".form-container");
+const search = document.querySelector(".search");
+const sortLinks = document.querySelectorAll(".sorting-list-link");
+
+if (search) {
+    formContainer.classList.add("form-container--closed");
+    search.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        formContainer.classList.toggle("form-container--closed");
+    });
+}
+
+
+if (sortLinks) {
+    console.log(sortLinks);
+    sortLinks.forEach(function(elem){
+        elem.addEventListener("click", function(evt){
+            evt.preventDefault();
+            document.querySelector(".sorting-list-link-active").classList.remove("sorting-list-link-active");
+            this.classList.add("sorting-list-link-active");
+        });
+    });
+}
