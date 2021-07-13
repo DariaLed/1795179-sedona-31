763668d0744638451faaaa35761f4b63de401639
DariaLed <84290710+DariaLed@users.noreply.github.com>
2021-07-13 21:17:54 +0300

    2 attempt

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const formContainer = document.querySelector(".form-container");
 const search = document.querySelector(".search");
 const sortLinks = document.querySelectorAll(".sorting-list-link");
+const arrivalDate = document.querySelector(".arrival-date");
+const departureDate = document.querySelector(".departure-date");
+const appointmentDate = document.querySelector(".appointment-date");
+
 
 if (search) {
     formContainer.classList.add("form-container--closed");
@@ -17,5 +21,18 @@ if (sortLinks) {
             document.querySelector(".sorting-list-link-active").classList.remove("sorting-list-link-active");
             this.classList.add("sorting-list-link-active");
         });
+    });
+}
+
+arrivalDate.required = false;
+departureDate.required = false;
+if (appointmentDate) {
+    appointmentDate.addEventListener("submit", function(evt){
+        if (!arrivalDate.value || !departureDate.value) {
+            evt.preventDefault();
+            appointmentDate.classList.remove("form--error");
+            appointmentDate.offsetWidth = appointmentDate.offsetWidth;
+            appointmentDate.classList.add("form--error");
+        }
     });
 }
